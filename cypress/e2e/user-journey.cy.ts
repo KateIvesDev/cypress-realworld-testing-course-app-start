eba570e18ac9b3da7ad0fb05cb0e6a1a2307d08c
Kate Ives <katie.ives30@gmail.com>
2023-02-13 11:48:23 -0600

    add tests

---
+++ cypress/e2e/user-journey.cy.ts
@@ -0,0 +1,19 @@
+describe("User Journey", () => {
+    it("a user can find a course on the homepage and complete the course lessons", () => {
+        cy.visit("http://localhost:3000")
+        cy.getByData("course-0").find("a").eq(3).click()
+        cy.location("pathname").should("eq", "/testing-your-first-application")
+        cy.getByData("next-lesson-button").click()
+        cy.location("pathname").should("eq",
+        "/testing-your-first-application/app-install-and-overview")
+        cy.getByData("challenge-answer-0").click()
+        cy.getByData("next-lesson-button").should("exist").click()
+        cy.location("pathname").should("eq", "/testing-your-first-application/installing-cypress-and-writing-our-first-test")
+        cy.getByData("challenge-answer-0").click()
+        cy.getByData("next-lesson-button").should("exist").click()
+        cy.location("pathname").should("eq", "/testing-your-first-application/setting-up-data-before-each-test")
+        cy.getByData("challenge-answer-0").click()
+        cy.getByData("next-lesson-button").should("exist").click()
+        cy.location("pathname").should("eq", "/")
+    })
+})
